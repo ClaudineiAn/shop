2069aa5660725869ad2dd93dbf3898ef6f780913
ClaudineiAn <claudinei2092016@gmail.com>
2023-12-05 03:23:32 -0300

    Tests

--- src/components/Account.vue
+++ src/components/Account.vue
@@ -5,7 +5,7 @@
   <AccontIcon class="accont" />
   <div class="accountPopUp" style="display:none">
       <div class="accountData">
-        <img :src="profileImg" alt="Profile img">
+        <img :src="updateProfileImg" alt="Profile img">
           <CameraIcon class="cameraIcon" />
           <input id="upload" type="file" ref="fileInput" accept="image/*" style="display: none" />
           <div class="group">
