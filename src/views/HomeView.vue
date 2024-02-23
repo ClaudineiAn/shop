535464695e553235ecfa19266ecccd655890059b
ClaudineiAn <claudinei2092016@gmail.com>
2024-02-22 22:18:31 -0300

    Tests

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -105,7 +105,7 @@ export default {
             </stop>
           </linearGradient><path fill="url(#gradient)" d="M12.406 13.844c1.188 0 2.156 0.969 2.156 2.156s-0.969 2.125-2.156 2.125-2.125-0.938-2.125-2.125 0.938-2.156 2.125-2.156zM12.406 8.531c7.063 0 12.156 6.625 12.156 6.625 0.344 0.438 0.344 1.219 0 1.656 0 0-5.094 6.625-12.156 6.625s-12.156-6.625-12.156-6.625c-0.344-0.438-0.344-1.219 0-1.656 0 0 5.094-6.625 12.156-6.625zM12.406 21.344c2.938 0 5.344-2.406 5.344-5.344s-2.406-5.344-5.344-5.344-5.344 2.406-5.344 5.344 2.406 5.344 5.344 5.344z"></path></svg>`
                 template+="</div></div>"
-                template+="<div></div>"
+                template+="<div></div></div>"
                 templatePopup+="<div class='view' style='display:none'>"
                 templatePopup+="<div>"
                 templatePopup+=`<img src="${require(`@/assets/images/${res.data[count].imagem.split(', ')[0]}`)}" />`
@@ -235,7 +235,6 @@ export default {
                 templatePopup+="</div>"
                 templatePopup+="<div>"
                 templatePopup+=`<svg xmlns="http://www.w3.org/2000/svg" width="63.031" height="30.768" viewBox="0 0 63.031 46.768" class="options"><linearGradient id="gradient"><stop offset="0%" stop-color="#F8EB6B"><animate attributeName="stop-color" values="#F8EB6B; #453100; #F8EB6B" dur="5s" repeatCount="indefinite"></animate></stop></linearGradient><g id="shopping-cart-1989" transform="translate(566.599 540.01)"><g id="Grupo_3" data-name="Grupo 3" transform="translate(-566.599 -540.01)"><path id="Caminho_8" fill="url(#gradient)" data-name="Caminho 8" d="M384.753,355.806a3.144,3.144,0,0,0-2.541-1.273h-47.64l-.587-2.26a4.448,4.448,0,0,0-4.3-3.329H323.21a.855.855,0,1,0,0,1.711h6.471a2.737,2.737,0,0,1,2.648,2.048l7.78,29.96a.855.855,0,0,0,.828.641h36.374a.855.855,0,1,0,0-1.711H341.6l-1.589-6.116h36.935a4.467,4.467,0,0,0,4.259-3.182l4.048-13.692A3.139,3.139,0,0,0,384.753,355.806Zm-1.14,2.311-4.048,13.692a2.746,2.746,0,0,1-2.619,1.956h-37.38l-4.55-17.522h47.2a1.46,1.46,0,0,1,1.4,1.873Z" transform="translate(-322.354 -348.944)"></path><path id="Caminho_9" fill="url(#gradient)" data-name="Caminho 9" d="M788.239,1211.888a4.912,4.912,0,1,1,4.913-4.913A4.918,4.918,0,0,1,788.239,1211.888Zm0-8.114a3.2,3.2,0,1,0,3.2,3.2A3.205,3.205,0,0,0,788.239,1203.774Z" transform="translate(-763.365 -1165.12)"></path><path id="Caminho_10" fill="url(#gradient)" data-name="Caminho 10" d="M1316.04,1211.888a4.912,4.912,0,1,1,4.913-4.913A4.918,4.918,0,0,1,1316.04,1211.888Zm0-8.114a3.2,3.2,0,1,0,3.2,3.2A3.205,3.205,0,0,0,1316.04,1203.774Z" transform="translate(-1268.31 -1165.12)"></path></g></g></svg><span></span>`
-                templatePopup+="</div>"
                 templatePopup+="</div>"
                 templatePopup+="</div>"
                 templatePopup+="</div>"
