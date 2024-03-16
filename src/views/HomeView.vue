032fe60a1710e3ce1711ee5cbcafe16a601e089e
ClaudineiAn <claudinei2092016@gmail.com>
2024-03-16 19:11:23 -0300

    Tests

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -263,19 +263,19 @@ export default {
           document.querySelector(".banner div.box>div.data").innerHTML=template
           document.querySelector(".banner div.box>div.popup").innerHTML=templatePopup
           document.querySelectorAll(".popup > .view > div:last-child > div:nth-child(3) > div:first-child > div > div:first-child > div:last-child > div:first-child").forEach(element => element.addEventListener("mouseenter", function(event) {
-            event.target.parentNode.firstElementChild.setAttribute("data-val","5.0")
+            event.target.parentNode.parentNode.firstElementChild.setAttribute("data-val","5")
           }));
           document.querySelectorAll(".popup > .view > div:last-child > div:nth-child(3) > div:first-child > div > div:first-child > div:last-child > div:nth-child(2)").forEach(element => element.addEventListener("mouseenter", function(event) {
-            event.target.parentNode.firstElementChild.setAttribute("data-val","4.0")
+            event.target.parentNode.parentNode.firstElementChild.setAttribute("data-val","4")
           }));
           document.querySelectorAll(".popup > .view > div:last-child > div:nth-child(3) > div:first-child > div > div:first-child > div:last-child > div:nth-child(3)").forEach(element => element.addEventListener("mouseenter", function(event) {
-            event.target.parentNode.firstElementChild.setAttribute("data-val","3.0")
+            event.target.parentNode.parentNode.firstElementChild.setAttribute("data-val","3")
           }));
           document.querySelectorAll(".popup > .view > div:last-child > div:nth-child(3) > div:first-child > div > div:first-child > div:last-child > div:nth-child(4)").forEach(element => element.addEventListener("mouseenter", function(event) {
-            event.target.parentNode.firstElementChild.setAttribute("data-val","2.0")
+            event.target.parentNode.parentNode.firstElementChild.setAttribute("data-val","2")
           }));
           document.querySelectorAll(".popup > .view > div:last-child > div:nth-child(3) > div:first-child > div > div:first-child > div:last-child > div:last-child").forEach(element => element.addEventListener("mouseenter", function(event) {
-            event.target.parentNode.firstElementChild.setAttribute("data-val","1.0")
+            event.target.parentNode.parentNode.firstElementChild.setAttribute("data-val","1")
           }));
           document.querySelectorAll(".popup > .view > div:last-child > div:nth-child(3) > div:first-child > div > div:first-child").forEach(element => element.addEventListener("mouseleave", function(event) {
             const parent = event.target.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode;
