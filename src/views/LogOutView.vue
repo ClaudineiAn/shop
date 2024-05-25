8eb866ef8c9d102e920d1c2751c3f1eceebffbbe
ClaudineiAn <claudinei2092016@gmail.com>
2024-05-25 03:08:52 -0300

    Tests

--- src/views/LogOutView.vue
+++ src/views/LogOutView.vue
@@ -4,10 +4,7 @@ import Cookies from 'js-cookie'
 
 import {checkIfLogged} from './HeaderView.vue'
 
-Cookies.set('id', undefined)
-Cookies.set('email', undefined)
 Cookies.set('name', undefined)
-Cookies.set('typeProfile', undefined)
 
 checkIfLogged()
 
