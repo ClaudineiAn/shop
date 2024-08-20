a969cb432d7861d11ebaff43f241ad67510c89a5
ClaudineiAn <claudinei2092016@gmail.com>
2024-08-20 15:29:43 -0300

    Tests

--- src/assets/js/access.js
+++ src/assets/js/access.js
@@ -19,44 +19,34 @@ export const validateUsername = (username, setError) => {
 
 export const validation = async (router, username, setError) => {
   validateUsername(username, setError);
-  if (document.querySelector("#errorAccess").innerHTML!=="") {
+  if (document.querySelector("#errorAccess").innerHTML !== "") {
     return;
   }
 
-  // Check if MetaMask is installed
   if (typeof window.ethereum !== 'undefined') {
-    console.log("a");
-    // Create a new Ethers provider using the MetaMask provider
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const provider = new ethers.providers.Web3Provider(window.ethereum, 'any');
     const signer = provider.getSigner();
-    console.log("b");
 
     try {
-      // Request account access if needed
       await provider.send('eth_requestAccounts', []);
-      const abi = JSON.parse(fs.readFileSync('./abi.json', 'utf8'));
-      console.log(0);
+      const response = await fetch('/path/to/abi.json');
+      const abi = await response.json();
+
       const contractAddress = "0xDA0bab807633f07f013f94DD0E6A4F96F8742B53";
       const userAuthContract = new ethers.Contract(contractAddress, abi, signer);
-      console.log(1);
 
       const accounts = await provider.listAccounts();
-      console.log(2);
-
       if (!accounts || accounts.length === 0) {
         await router.push('/access?error=No accounts found. Please login to MetaMask.');
         return;
       }
-      console.log(3);
-      console.log('Contract ABI:', abi);
-      
+
       const registeredUsername = await userAuthContract.getUser();
-      console.log(4);
 
       if (!registeredUsername) {
         if (confirm('You are about to create a new account. Is this what you would like?')) {
           const tx = await userAuthContract.register(username);
-          await tx.wait(); // Wait for the transaction to be mined
+          await tx.wait();
           const logged = await makeLog(username);
           if (logged === 200) {
             await router.push('/');
@@ -72,7 +62,7 @@ export const validation = async (router, username, setError) => {
       await router.push('/access?error=' + error.message);
     }
   } else {
-    document.querySelector(".overlay").style.display="flex";
+    document.querySelector(".overlay").style.display = "flex";
   }
 };
 
