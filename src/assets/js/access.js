758f6738b229f85586cf801236345dd7ff7a4247
ClaudineiAn <claudinei2092016@gmail.com>
2024-08-21 02:21:36 -0300

    Tests

--- src/assets/js/access.js
+++ src/assets/js/access.js
@@ -28,41 +28,46 @@ export const validation = async (router, username, setError) => {
   if (document.querySelector("#errorAccess").innerHTML !== "") {
     return;
   }
-console.log(0)
+  
+  console.log(0)
   if (typeof window.ethereum !== 'undefined') {
-    const provider = new ethers.BrowserProvider(window.ethereum);
-    const signer = await provider.getSigner();
-    const contractAddress = "0x2f9Ce96F9A899363D061096BBA3e81B67d977aE8";
-
-console.log(1)
-    const isContractDeployed = await checkContractDeployment(provider, contractAddress);
-    if (!isContractDeployed) {
-      console.error('Contract not deployed at this address.');
-      await router.push('/access?error=Contract not deployed at this address.');
-      return;
-    }
-	
-console.log(2)
-
     try {
+      console.log('Creating provider...');
+      const provider = new ethers.BrowserProvider(window.ethereum);
+  
+      console.log('Getting signer...');
+      const signer = await provider.getSigner();
+  
+      console.log('Checking contract deployment...');
+      const contractAddress = "0x2f9Ce96F9A899363D061096BBA3e81B67d977aE8";
+      const isContractDeployed = await checkContractDeployment(provider, contractAddress);
+      
+      if (!isContractDeployed) {
+        console.error('Contract not deployed at this address.');
+        await router.push('/access?error=Contract not deployed at this address.');
+        return;
+      }
+      
+      console.log('Requesting accounts...');
       await provider.send('eth_requestAccounts', []);
-
+  
+      console.log('Creating contract instance...');
       const userAuthContract = new ethers.Contract(contractAddress, abi, signer);
-
-console.log(3)
+  
+      console.log('Listing accounts...');
       const accounts = await provider.listAccounts();
       if (!accounts || accounts.length === 0) {
         await router.push('/access?error=No accounts found. Please login to MetaMask.');
         return;
       }
-	  
-console.log(4)
-
+      
+      console.log('Getting registered username...');
       const registeredUsername = await userAuthContract.getUser();
       console.log('Registered username:', registeredUsername);
-
+  
       if (!registeredUsername) {
         if (confirm('You are about to create a new account. Is this what you would like?')) {
+          console.log('Registering new username...');
           const tx = await userAuthContract.register(username);
           await tx.wait();
           const logged = await makeLog(username);
