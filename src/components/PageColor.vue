cc11a993f0861903ae25cde57e640219196dafa2
ClaudineiAn <claudinei2092016@gmail.com>
2024-09-10 07:53:54 +0100

    Tests

--- src/components/PageColor.vue
+++ src/components/PageColor.vue
@@ -1,58 +1,60 @@
 <template>
-  </v-container>	<!-- Icon to open popup -->
-	<v-icon @click="showPopup = true">mdi-star</v-icon>
+  <div>
+    <!-- Icon to open popup -->
+    <v-icon @click="showPopup = true">mdi-star</v-icon>
 
-	<!-- Popup -->
-	<v-dialog v-model="showPopup" persistent max-width="200" max-height="200">
-	  <v-card>
-		<v-card-title>
-		  <!-- Reset button -->
-		  <v-btn @click="resetColor" small>Reset</v-btn>
+    <!-- Popup -->
+    <v-dialog v-model="showPopup" persistent max-width="200" max-height="200">
+      <v-card>
+        <v-card-title>
+          <!-- Reset button -->
+          <v-btn @click="resetColor" small>Reset</v-btn>
 
-		  <!-- Down arrow with dropdown -->
-		  <v-menu
-			activator="parent"
-			:close-on-content-click="false"
-			v-slot:activator="{ on, attrs }"
-		  >
-			<v-btn small icon v-bind="attrs" v-on="on">
-			  <v-icon>mdi-menu-down</v-icon>
-			</v-btn>
-			<v-list>
-			  <v-list-item v-for="(item, index) in dropdownItems" :key="index">
-				<v-list-item-title>{{ item }}</v-list-item-title>
-			  </v-list-item>
-			</v-list>
-		  </v-menu>
-		</v-card-title>
+          <!-- Down arrow with dropdown -->
+          <v-menu
+            activator="parent"
+            :close-on-content-click="false"
+            v-slot:activator="{ on, attrs }"
+          >
+            <v-btn small icon v-bind="attrs" v-on="on">
+              <v-icon>mdi-menu-down</v-icon>
+            </v-btn>
+            <v-list>
+              <v-list-item v-for="(item, index) in dropdownItems" :key="index">
+                <v-list-item-title>{{ item }}</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </v-card-title>
 
-		<v-card-text>
-		  <!-- Central SVG icon -->
-		  <div class="text-center">
-			<svg :fill="selectedColor" width="150" height="150" viewBox="0 0 24 24">
-			  <path d="M12 2L2 22h20L12 2zm0 3.3L18.74 20H5.26L12 5.3z" />
-			</svg>
-		  </div>
+        <v-card-text>
+          <!-- Central SVG icon -->
+          <div class="text-center">
+            <svg :fill="selectedColor" width="150" height="150" viewBox="0 0 24 24">
+              <path d="M12 2L2 22h20L12 2zm0 3.3L18.74 20H5.26L12 5.3z" />
+            </svg>
+          </div>
 
-		  <!-- Gradient Color Bar -->
-		  <div class="color-bar mt-5">
-			<input
-			  type="range"
-			  min="0"
-			  max="360"
-			  v-model="hue"
-			  @input="updateColor"
-			  class="gradient-slider"
-			/>
-		  </div>
+          <!-- Gradient Color Bar -->
+          <div class="color-bar mt-5">
+            <input
+              type="range"
+              min="0"
+              max="360"
+              v-model="hue"
+              @input="updateColor"
+              class="gradient-slider"
+            />
+          </div>
 
-		  <!-- Save button -->
-		  <div v-if="selectedColor !== originalColor" class="text-center mt-2">
-			<v-btn color="primary" @click="saveColor">Save</v-btn>
-		  </div>
-		</v-card-text>
-	  </v-card>
-	</v-dialog>
+          <!-- Save button -->
+          <div v-if="selectedColor !== originalColor" class="text-center mt-2">
+            <v-btn color="primary" @click="saveColor">Save</v-btn>
+          </div>
+        </v-card-text>
+      </v-card>
+    </v-dialog>
+  </div>
 </template>
 
 <script>
