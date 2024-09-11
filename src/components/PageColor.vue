62765780e54a4aee6c6c06144ccf68c44c658b5e
ClaudineiAn <claudinei2092016@gmail.com>
2024-09-11 20:59:18 +0100

    Tests

--- src/components/PageColor.vue
+++ src/components/PageColor.vue
@@ -57,13 +57,16 @@
           <!-- Central SVG icon -->
           <div class="text-center">
 			<svg width="150px" height="150px" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
-					<linearGradient id="gradient">
-					<stop offset="0%" stop-color="var(--color1)">
-					  <animate attributeName="stop-color" :values="selectedColor"; :values="selectedColor2"; :values="selectedColor"" dur="5s" repeatCount="indefinite" />
-					</stop>
-				  </linearGradient>
-				<path fill="url(#gradient)" d="M0 5.04V4l4-4h8l4 4v1.04L8 16 0 5.04zM2 5l6 8.5L4 5H2zm12 0h-2l-4 8.5L14 5zM6 5l2 6 2-6H6zM4 2L2 4h2l2-2H4zm8 0h-2l2 2h2l-2-2zM7 2L6 4h4L9 2H7z" fill-rule="evenodd"/>
+			  <defs>
+				<linearGradient id="gradient">
+				  <stop offset="0%" :stop-color="selectedColor">
+					<animate attributeName="stop-color" :values="selectedColor + ';' + selectedColor2" dur="5s" repeatCount="indefinite" />
+				  </stop>
+				</linearGradient>
+			  </defs>
+			  <path fill="url(#gradient)" d="M0 5.04V4l4-4h8l4 4v1.04L8 16 0 5.04zM2 5l6 8.5L4 5H2zm12 0h-2l-4 8.5L14 5zM6 5l2 6 2-6H6zM4 2L2 4h2l2-2H4zm8 0h-2l2 2h2l-2-2zM7 2L6 4h4L9 2H7z" fill-rule="evenodd" />
 			</svg>
+
           </div>
 
           <!-- Gradient Color Bar -->
