700fd37f4931ceef5ac4c7b68f2e3f9bf948734f
ClaudineiAn <claudinei2092016@gmail.com>
2024-09-11 21:31:07 +0100

    Tests

--- src/components/PageColor.vue
+++ src/components/PageColor.vue
@@ -58,13 +58,13 @@
           <div class="text-center">
 			<svg width="150px" height="150px" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
 			  <defs>
-				<linearGradient id="grad">
+				<linearGradient id="gradColor">
 				  <stop offset="0%" :stop-color="selectedColor">
-					<animate attributeName="stop-color" :values="selectedColor + ';' + selectedColor2" dur="5s" repeatCount="indefinite" />
+					<animate attributeName="stop-color" :values="selectedColor + ';' + selectedColor2 + ';' + selectedColor" dur="7.5s" repeatCount="indefinite" />
 				  </stop>
 				</linearGradient>
 			  </defs>
-			  <path fill="url(#grad)" d="M0 5.04V4l4-4h8l4 4v1.04L8 16 0 5.04zM2 5l6 8.5L4 5H2zm12 0h-2l-4 8.5L14 5zM6 5l2 6 2-6H6zM4 2L2 4h2l2-2H4zm8 0h-2l2 2h2l-2-2zM7 2L6 4h4L9 2H7z" fill-rule="evenodd" />
+			  <path fill="url(#gradColor)" d="M0 5.04V4l4-4h8l4 4v1.04L8 16 0 5.04zM2 5l6 8.5L4 5H2zm12 0h-2l-4 8.5L14 5zM6 5l2 6 2-6H6zM4 2L2 4h2l2-2H4zm8 0h-2l2 2h2l-2-2zM7 2L6 4h4L9 2H7z" fill-rule="evenodd" />
 			</svg>
 
           </div>
