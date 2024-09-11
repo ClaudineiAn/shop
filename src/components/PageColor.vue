d6e608cc2e7be282a5096ad798c261bbdf00dc0e
ClaudineiAn <claudinei2092016@gmail.com>
2024-09-11 23:05:03 +0100

    Tests

--- src/components/PageColor.vue
+++ src/components/PageColor.vue
@@ -21,9 +21,9 @@
 
       <!-- Popup content that opens directly below the icon -->
       <v-card>
-        <v-card-title>
+        <v-card-title class="btnBox">
           <!-- Reset button -->
-          <v-btn @click="resetColor" small>Reset</v-btn>
+          <v-btn class="reset" @click="resetColor" small>Reset</v-btn>
 
             <v-container>
 				<!-- Dropdown Menu with vertical three-dots icon and animation -->
@@ -36,8 +36,17 @@
 				>
 				  <!-- Activator: Three-dots vertical icon button -->
 				  <template v-slot:activator="{ on, attrs }">
-					<v-btn small icon v-bind="attrs" v-on="on" @click="dropdownOpen">
-					  <v-icon>mdi-dots-vertical</v-icon> <!-- Vertical three-dots icon -->
+					<v-btn class="doneBtn" small icon v-bind="attrs" v-on="on" @click="dropdownOpen">
+					  <svg width="800px" height="800px" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+						<linearGradient id="gradientUp">
+						  <stop offset="0%" :stop-color="selectedColor">
+							<animate attributeName="stop-color" :values="selectedColor + ';' + selectedColor2 + ';' + selectedColor" dur="5s" repeatCount="indefinite" />
+						  </stop>
+						</linearGradient>
+						<path fill="url(#gradientUp)" d="M8 12C9.10457 12 10 12.8954 10 14C10 15.1046 9.10457 16 8 16C6.89543 16 6 15.1046 6 14C6 12.8954 6.89543 12 8 12Z"/>
+						<path fill="url(#gradientUp)" d="M8 6C9.10457 6 10 6.89543 10 8C10 9.10457 9.10457 10 8 10C6.89543 10 6 9.10457 6 8C6 6.89543 6.89543 6 8 6Z"/>
+						<path fill="url(#gradientUp)" d="M10 2C10 0.89543 9.10457 -4.82823e-08 8 0C6.89543 4.82823e-08 6 0.895431 6 2C6 3.10457 6.89543 4 8 4C9.10457 4 10 3.10457 10 2Z"/>
+					  </svg>
 					</v-btn>
 				  </template>
 
@@ -60,7 +69,7 @@
 			  <defs>
 				<linearGradient id="gradColor">
 				  <stop offset="0%" :stop-color="selectedColor">
-					<animate attributeName="stop-color" :values="selectedColor + ';' + selectedColor2 + ';' + selectedColor" dur="7.5s" repeatCount="indefinite" />
+					<animate attributeName="stop-color" :values="selectedColor + ';' + selectedColor2 + ';' + selectedColor" dur="5s" repeatCount="indefinite" />
 				  </stop>
 				</linearGradient>
 			  </defs>
@@ -86,7 +95,7 @@
             v-if="showSaveButton"
             class="text-center mt-2"
           >
-            <v-btn color="primary" @click="saveColor">Save</v-btn>
+            <v-btn class="save" color="primary" @click="saveColor">Save</v-btn>
           </div>
         </v-card-text>
       </v-card>
